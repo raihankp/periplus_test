49e4e49fc97f148f5e2c98362f5ba5bbe430f935
Raihan Kus Putranto <raihan.kus@ui.ac.id>
2025-05-22 16:03:47 +0700

    refactor: remove relogin in every method, refactor it to BeforeClasss, and add testLogoutConsistency in AddToCartTest

--- Openway_Periplus/src/test/java/com/periplus/base/BaseTest.java
+++ Openway_Periplus/src/test/java/com/periplus/base/BaseTest.java
@@ -4,7 +4,9 @@ import com.base.BasePage;
 import com.periplus.pages.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
@@ -19,17 +21,22 @@ public class BaseTest {
     protected CartPage cartPage;
     private String url = "https://www.periplus.com";
 
-    @BeforeMethod
-    public void setUp() {
+    @BeforeClass
+    public void setUpClass() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(url);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         homePage = new HomePage(driver);
+
+        // Login once
+        loginPage = homePage.clickLoginLink();
+        accountPage = loginPage.login("raihankp96@gmail.com", "raihankp123");
+        homePage = accountPage.clickHomePage();
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown() {
         driver.quit();
     }
--- Openway_Periplus/src/test/java/com/periplus/tests/AddToCartTest.java
+++ Openway_Periplus/src/test/java/com/periplus/tests/AddToCartTest.java
@@ -7,12 +7,8 @@ import org.testng.annotations.Test;
 public class AddToCartTest extends BaseTest {
     @Test(description = "TC_ADDTOCART_001 - Add product to Cart by Hover")
     public void addToCartByHover() {
-        loginPage = homePage.clickLoginLink();
-        accountPage = loginPage.login("raihankp96@gmail.com", "raihankp123");
-        homePage = accountPage.clickHomePage();
-
         homePage.HoverProductTitle(1);
-        homePage.clickAddToCart(1);
+        homePage.clickAddToCartByIndex(1);
         String expectedBookTitle = homePage.getBookTitleAdded(1);
 
         homePage.hoverCartIcon();
@@ -21,19 +17,30 @@ public class AddToCartTest extends BaseTest {
 
     @Test(description = "TC_ADDTOCART_002 - Add product to Cart by Product Detail Page")
     public void addToCartByProductDetailPage() {
-        loginPage = homePage.clickLoginLink();
-        accountPage = loginPage.login("raihankp96@gmail.com", "raihankp123");
-        homePage = accountPage.clickHomePage();
-        String expectedBookTitle = homePage.getBookTitleAdded(1);
-        productDetailPage = homePage.clickProductTitle(1);
+        String expectedBookTitle = homePage.getBookTitleAdded(2);
+        productDetailPage = homePage.clickProductTitle(2);
 
         productDetailPage.clickAddToCart();
-        String modalText = productDetailPage.getAddToCartModalText();
-        productDetailPage.waitForAddToCartModalToDisappear();
+        String modalText = productDetailPage.getAndWaitForAddToCartModalText();
 
         productDetailPage.hoverCartIcon();
         Assert.assertEquals(modalText, "Success add to cart");
         Assert.assertTrue(productDetailPage.isBookTitleInCart(expectedBookTitle));
+    }
+
+    @Test(description = "TC_ADDTOCART_003 - Consistency If Product Still in Cart After Logout")
+    public void testLogoutConsistency() {
+        homePage.clickHomePage();
+        homePage.HoverProductTitle(3);
+        homePage.clickAddToCartByIndex(3);
+        String expectedBookTitle = homePage.getBookTitleAdded(3);
+        homePage.clickLogoutLink();
+
+        loginPage = homePage.clickLoginLink();
+        accountPage = loginPage.login("raihankp96@gmail.com", "raihankp123");
+
+        homePage.hoverCartIcon();
+        Assert.assertTrue(homePage.isBookTitleInCart(expectedBookTitle));
     }
 
 }
